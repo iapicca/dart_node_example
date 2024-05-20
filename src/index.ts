dc34eb58405576bd80105e3ba1106e1b6d6db14c
Francesco Iapicca <iapicca@email.com>
2024-05-20 16:44:37 +0300

    add js import

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
+import { greeting } from './greeting.js';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from TS!');
+  res.send(greeting());
 });
 
 app.listen(port, () => {
